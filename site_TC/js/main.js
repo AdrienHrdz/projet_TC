5f4ea726f2fb26a29de48d118a62a67a01c37334
Amandine <amandine.piccinali@gmail.com>
2022-03-22 17:08:43 +0100

    footerok

--- site_TC/js/main.js
+++ site_TC/js/main.js
@@ -32,25 +32,24 @@ document.querySelector("header").innerHTML = `
 document.querySelector("footer").innerHTML = `
             <div class=cadres>
                 <ul class=listcadre>
-                    <li class=cadre1>
+                    <li class=cl1>
                         <h4>Etat-Major</h4> 
+                        <p> 24, rue René Camphin </p>
                     </li>
 
-                    <li>
+                    <li class=cl2>
                         <h4>Horaires d'ouverture</h4>
-                
+                        <p> Lundi au Vendredi : 9h - 17h </p>
                     </li>
             
-                    <li>
-                       
-                        <h4>Contactez-nous</h4>
-                        
+                    <li class=cl3>
+                        <h4>Contactez-nous : </h4>
+                        <button id="bouton_twitter" onclick="window.location.href = 'https://twitter.com/PompiersFR?ref_src=twsrc%5Etfw%7Ctwcamp%5Eembeddedtimeline%7Ctwterm%5Eprofile%3APompiersfr%7Ctwgr%5EeyJ0ZndfZXhwZXJpbWVudHNfY29va2llX2V4cGlyYXRpb24iOnsiYnVja2V0IjoxMjA5NjAwLCJ2ZXJzaW9uIjpudWxsfSwidGZ3X2hvcml6b25fdHdlZXRfZW1iZWRfOTU1NSI6eyJidWNrZXQiOiJodGUiLCJ2ZXJzaW9uIjpudWxsfSwidGZ3X3NrZWxldG9uX2xvYWRpbmdfMTMzOTgiOnsiYnVja2V0IjoiY3RhIiwidmVyc2lvbiI6bnVsbH0sInRmd19zcGFjZV9jYXJkIjp7ImJ1Y2tldCI6Im9mZiIsInZlcnNpb24iOm51bGx9fQ%3D%3D&ref_url=https%3A%2F%2Fwww.pompiers.fr%2Fgrand-public';">Notre Twitter</button
                     </li>
 
-                    <li>
-                       
+                    <li class=cl4>
+                        <h4> Appelez au 18 ou 112 <br>______________________</h4>
                         <button onclick="topFunction()" id="upBtn" title="Retour au haut de la page">Retour Haut de page</button>
-                   
                     </li>
                 </ul>
             </div>
