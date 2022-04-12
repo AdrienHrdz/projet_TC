e677622fdec6ac182cf88ea4cd98dc882b247187
Cortoz890 <72150768+Cortoz890@users.noreply.github.com>
2022-04-12 17:40:59 +0200

    Add files via upload
    fin séance 12/04

--- matlab/Lidar_traitement.m
+++ matlab/Lidar_traitement.m
@@ -4,10 +4,10 @@ clc;
 
 %% Variables globales
 
-sigma = 0.05; % Largeur d'une ligne
+sigma = 0.01; % Largeur d'une ligne
 d = 0.01;      % Rayon max dans lequel se trouvent 2 points d'une même ligne
 n = 10;       % Nbs de points minimum dans une ligne
-gamma = 0.04; % Difference accepté pour regrouper 2 lignes
+gamma = 0.4; % Difference accepté pour regrouper 2 lignes
 
 %% Récupération des données
 
@@ -20,7 +20,6 @@ end
 
 [A,count]=fscanf(fid,'%g%g%g',[3 inf]); %sépare les données en 3 colonnes
 A(3,:)=[];%supprime la ligne qualité
-disp(A);%affichage matrice angle-distance
 
 %% Changement de bases
 
@@ -32,6 +31,7 @@ points = [X;Y];
 %% Traçage de lignes
 k = 1;
 lines = [];
+coeffsDir = [];
 
 while(k<length(X)-1) 
    flag = true;
@@ -54,17 +54,33 @@ while(k<length(X)-1)
    end
    
    if(isempty(line) == false)
-        lines = cat(1, lines, [line(1,:); line(end,:)]);
+       m = (line(end, 2) - line(1, 2))/(line(end, 1) - line(1, 1)); % Coeff directeur de la ligne en cours
+       coeffsDir = cat(1, coeffsDir, m);                            % Ajout du coeff dans la matrice lines
+       
+       lines = cat(1, lines, [line(1,:); line(end,:)]); % Ajout des coors des 2 point de la ligne dans la matrice lines
+  
+       
+       
    end
 end
 
+% for i = 2:2:length(lines)
+%     for j = 2:2:length(lines)
+%        if(i ~= j && abs(coeffsDir(i/2) - coeffsDir(j/2)) < gamma ...
+%                && abs(lines(i, 1) - lines(j, 1)) < gamma && abs(lines(i, 2) - lines(j, 2)) < gamma)
+%            lines(i,:) = []; 
+%            lines(i+1,:) = [];
+% 
+%        else
+%        end
+%     end
+%    
+% end
+
 %% Affichage
-scatter(X, Y); hold on;
-uhuh = size(lines);
-
 for i = 0:length(lines)/2 - 1
-    plot(lines(1+2*i:2*(i+1),1), lines(1+2*i:2*(i+1), 2));
-
+    plot(lines(1+2*i:2*(i+1),1), lines(1+2*i:2*(i+1), 2)); hold on;
+   
 end
 
 status=fclose(fid); %ferme fichier
