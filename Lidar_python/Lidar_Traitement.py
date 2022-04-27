ea8d208efd5664dcb0a642075bcd9baa276faadb
Cortoz890 <corto.deschamps@club-internet.fr>
2022-04-27 14:42:07 +0200

    def fonctions pour traitement Lidar

--- Lidar_python/Lidar_Traitement.py
+++ Lidar_python/Lidar_Traitement.py
@@ -1,86 +1,99 @@
+## Importation des modules ##
+
 import numpy as np
 import matplotlib.pyplot as plt
 from math import cos,sin,pi
 import circle as circle
 
-# Variables globales
-sigma = 0.01 # Largeur d'une ligne
-d = 0.01 # Rayon max dans lequel se trouvent 2 points d'une même ligne
-n = 5 # Nbs de points minimum dans une ligne
-gamma = 0.4 # Difference accepté pour regrouper 2 lignes
 
-# Récupération des données
+## Définition des fonctions ##
 
-#[A,count]=fscanf(fid,'%g%g%g',[3 inf]); %sépare les données en 3 colonnes
-#A(3,:)=[];%supprime la ligne qualité
+def getData(filePath):
+    '''
+    Récupére les données envoyés par le Lidar
+    '''
 
-x=np.array([])
-y=np.array([])
-#A.append(x[1]) B.append(x[2])
-with open('Lidar_python/RPLIDAR.txt','r') as fr: #Ouvre le fichier texte
-    linesTxt = fr.readlines()
-    ptr = 1
-    with open('RPLIDAR2.txt','w') as fw:
-        for lineTxt in linesTxt: #enlève les 3 premieres lignes du fichier et rempli dans RPLIDAR2.txt
-            if (ptr !=1 and ptr != 2 and ptr != 3):
-                l=lineTxt.split(' ')
-                x=np.append(x,l[0]) 
-                y=np.append(y,l[1])
-                fw.write(lineTxt)
-            ptr += 1
-        fw.close()
-    fr.close()
-A=np.concatenate([[x], [y]], axis=0)
-#Matrice ligne 1 : angle / ligne 2 : distance
+    x = np.array([])
+    y = np.array([])
 
-# Changement de bases
+    with open(filePath, 'r') as fr:       #Ouvre le fichier texte
+        linesTxt = fr.readlines()
+        ptr = 1
+        with open('Lidar_python/RPLIDAR2.txt','w') as fw:
+            for lineTxt in linesTxt:                        # Enlève les 3 premieres lignes du fichier et rempli dans RPLIDAR2.txt
+                if (ptr !=1 and ptr != 2 and ptr != 3):
+                    l=lineTxt.split(' ')
+                    x = np.append(x,l[0]) 
+                    y = np.append(y,l[1])
+                    fw.write(lineTxt)
+                ptr += 1
+            fw.close()
+        fr.close()
 
-#X=A(2,:).*cos(A(1,:).*(pi/180))./1000;
-#Y=A(2,:).*sin(A(1,:).*(pi/180))./1000;
+    A = np.concatenate([[x], [y]], axis=0)
 
-X=np.array([])
-Y=np.array([])
-for (dist,angle) in zip(A[0,:],A[1,:]) :
-    dist_i=float(dist)
-    angle_i=float(angle)
-    b1=angle_i*cos(dist_i*pi/180)/1000
-    b2=angle_i*sin(dist_i*pi/180)/1000
-    X=np.append(X,b1)
-    Y=np.append(Y,b2)
+    return A
 
-#points = np.concatenate([[X], [Y]], axis=0)
 
-#Traçage de lignes
-k = 0
-lines = np.empty((1, 2), int)
-coeffsDir = np.empty((1, 1), int)
+def changeBase(A):
+    '''
+    Passage des données en une base (distance, angle) en une base (x, y)
+    '''
+    #A = getData()
+    X = np.array([])
+    Y = np.array([])
+    for (dist,angle) in zip(A[0,:],A[1,:]) :
+        dist_i=float(dist)
+        angle_i=float(angle)
+        b1=angle_i*cos(dist_i*pi/180)/1000
+        b2=angle_i*sin(dist_i*pi/180)/1000
+        X=np.append(X, b1)
+        Y=np.append(Y, b2)
 
-while(k<len(X)-1) :
-    flag = True
-    line = np.empty((1, 2), int)
+    return X, Y
+
+
+def createLines(X, Y):
+    '''
+    Crée les lignes à partir du nuage de point contenu dans X, Y
+    '''
+    #X = changeBase()[0]
+    #Y = changeBase()[1]
+
+    k = 0
+    d = 0.01        # Rayon max dans lequel se trouvent 2 points d'une même ligne
+    n = 5           # Nbs de points minimum dans une ligne
+
+    lines = np.empty((1, 2), int)
+    coeffsDir = np.empty((1, 1), int)
+
+    while(k<len(X)-1) :
+        flag = True
+        line = np.empty((1, 2), int)
+        
     
-   
-    while(flag and k < len(X)-1) : 
-        (Cx, Cy) = circle.circle(X[k], Y[k], d)
-            
-        if((X[k+1] - X[k])**2 + (Y[k+1] - Y[k])**2 < d**2) :
-            line = np.append(line, [[ X[k+1], Y[k+1] ]], axis=0)
+        while(flag and k < len(X)-1) : 
+            #(Cx, Cy) = circle.circle(X[k], Y[k], d)
                 
-        else :
-            flag = False
+            if((X[k+1] - X[k])**2 + (Y[k+1] - Y[k])**2 < d**2) :
+                line = np.append(line, [[ X[k+1], Y[k+1] ]], axis=0)
+                    
+            else :
+                flag = False
+                
+            k += 1
+    
+        if(len(line) > n+1) :
             
-        k += 1
-  
-    if(len(line) > n+1) :
-        
-        line = np.delete(line, 0, axis=0)
-        
-        m = (line[-1, 1] - line[0, 1])/(line[-1, 0] - line[0, 0])
-        coeffsDir = np.append(coeffsDir, [[m]], axis=0) 
-        
-                            
-        lines = np.append(lines, [ line[0,:], line[-1,:] ], axis=0);         
+            line = np.delete(line, 0, axis=0)
+            
+            m = (line[-1, 1] - line[0, 1])/(line[-1, 0] - line[0, 0])
+            coeffsDir = np.append(coeffsDir, [[m]], axis=0) 
+            
+                                
+            lines = np.append(lines, [ line[0,:], line[-1,:] ], axis=0)         
 
+    return lines
 
 
 #% for i = 2:2:len(lines)
@@ -97,11 +110,25 @@ while(k<len(X)-1) :
 #% end
 #
 
-#Affichage
-lines = np.delete(lines, 0, axis=0)
 
-for i in range(int(len(lines)/2)):
-    plt.plot([ lines[2*i, 0], lines[2*i+1, 0] ], [ lines[2*i, 1], lines[2*i+1, 1] ])
+def printLines(lines):
+    '''
+    Affiche la carte sous forme de lignes
+    '''
+    lines = np.delete(lines, 0, axis=0)
 
-plt.show()
-#status=fclose(fid); #ferme fichier
+    for i in range(int(len(lines)/2)):
+        plt.plot([ lines[2*i, 0], lines[2*i+1, 0] ], [ lines[2*i, 1], lines[2*i+1, 1] ])
+
+    plt.show()
+
+
+## Tests ##
+A = getData('Lidar_python/RPLIDAR.txt')
+
+X = changeBase(A)[0]
+Y = changeBase(A)[1]
+
+lines = createLines(X, Y)
+
+printLines(lines)
